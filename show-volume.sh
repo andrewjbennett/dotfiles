5851473942b4c69fb4d3a0cfeceaa110f1ef2854
Andrew Bennett <andrew.bennett@unsw.edu.au>
2016-12-05 23:37:42 +1100

    volume w/ fa

--- show-volume.sh
+++ show-volume.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-amixer get Master | egrep -o "[0-9]+%"
+volume=$(amixer get Master | egrep -o "[0-9]+%" | tr -d '%')
+
+if [[ $volume -lt 1 ]]; then
+    echo -n '<fn=1></fn>'
+elif [[ $volume -lt 30 ]]; then
+    echo -n '<fn=1></fn>'
+else
+    echo -n '<fn=1></fn>'
+fi
+echo " $volume%"
+
+exit
+
+
+
+if [[ $volume -lt 10 ]]; then
+    echo -n '<fn=1></fn>'
+elif [[ $volume -lt 25 ]]; then
+    echo -n '<fn=1></fn>'
+elif [[ $volume -lt 51 ]]; then
+    echo -n '<fn=1></fn>'
+elif [[ $volume -lt 80 ]]; then
+    echo -n '<fn=1></fn>'
+else
+    echo -n '<fn=1><</fn>'
+fi
+
+echo " $volume%"
